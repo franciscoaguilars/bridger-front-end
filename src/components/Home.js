25b51e71ddaf3d6be057f3e7490c3f4cb68262cc
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-11-19 12:01:09 -0600

    removed list from homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,27 +2,26 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Home = () => {
-  const [students, setStudents] = useState([])
+  // const [students, setStudents] = useState([])
 
-  useEffect(()=> {
-    const url = 'http://localhost:3000/api/v1/students.json'
-    axios.get(url)
-    .then( resp => setStudents(resp.data) )
-    .catch( resp => console.log(resp) )
-  }, [students.length])
+  // useEffect(()=> {
+  //   const url = 'http://localhost:3000/api/v1/students.json'
+  //   axios.get(url)
+  //   .then( resp => setStudents(resp.data) )
+  //   .catch( resp => console.log(resp) )
+  // }, [students.length])
 
-  const list = students.map( student => {
-    return (
-      <ul>
-        <li key={student.id}>{student.first_name}</li>
-      </ul>
-    )
-  })
+  // const list = students.map( student => {
+  //   return (
+  //     <ul>
+  //       <li key={student.id}>{student.first_name}</li>
+  //     </ul>
+  //   )
+  // })
 
   return (
     <div>
       Homepage!
-      {list}
     </div>
   )
 }
