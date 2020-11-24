3111a6faa50cf8b0e89eb899d42a47691089f30f
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-11-24 15:35:22 -0600

    worked on the tutor card and the partner card and rendering grid of cards in tutors and partners pages

--- src/components/Tutors/Tutor.js
+++ src/components/Tutors/Tutor.js
@@ -1,10 +1,18 @@
 import react from 'react';
 
 
-const Tutor = () => {
+const Tutor = (props) => {
   return (
-    <div>
-      Tutor!
+    <div className="tutor-card">
+      <div className="tutor-card-image">
+
+      </div>
+      <div className="tutor-card-details">
+        <h5>{`${props.attributes.first_name} ${props.attributes.last_name}`}</h5>
+        <small><strong>{props.attributes.occupation}</strong></small>
+        <small>{`${props.attributes.city}, ${props.attributes.state}, ${props.attributes.country}`}</small>
+        <small><em>{props.attributes.linked_in_link}</em></small>
+      </div>
     </div>
   )
 }
