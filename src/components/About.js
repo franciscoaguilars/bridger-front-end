41f0587e401c854c49b1dea4defc385f6b14e09b
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-12-22 13:03:34 -0700

    improved styling for informational section of the website, and added copy to about me page

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,67 @@ import react from 'react';
 
 const About = () => {
   return (
-    <div className="container">
-      <h1 className="mb-4">Bridging the Education Gap...</h1>
-      <p>By bringing free tutoring and mentorship to 9-12 students who can't afford to pay</p>
+    <div>
+      <div className="about-banner">
+      <div className="about-banner-overlay">
+        <div className="about-banner-overlay-header">Tutoring For A Fair Future</div>
+      </div>
+      </div>
+        <div className="container">
+        <div className="mb-5">
+          <div className="row mb-4">
+            <div className="col-lg-4 col-sm-12 d-flex align-items-center d-flex align-items-center">
+              <h1 className="mb-4">What Is Smart Reach?</h1>
+            </div>
+            <div className="col-lg-8 col-sm-12">
+              <p>Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike. In the wake of the COVID-19 pandemic and ever increasing wage gaps, students who are disadvantaged have more barriers to their success than ever, with students from lower socioeconomic status communities consistently scoring behind their more privileged peers. Adding to this pressure, now the interpersonal knowledge that fosters curiosity and an interest in learning is no longer present. It is our goal to empower these students and make resources available to them so that they may meet their potential regardless of the circumstances they find themselves at the current moment.
+            </p>
+            </div>
+          </div>
+          <div className="row mb-4">
+            <div className="col-lg-8 col-sm-12">
+              <p>We are a group of individuals who aim to utilize the skills and knowledge we have gained through our personal educational achievement to bridge the gap in quality education through free tutoring services for students that are not sufficiently served by their current education systems. We hope to work with these students to create equality between all communities in the strength and quality of the education that young minds receive. We are qualified to serve each student with an individualistic and holistic approach as a result of our tutors’ expansive educational experiences, JEDI (Justice, Equity, Diversity, and Inclusivity) training, and thorough knowledge of how to navigate college and graduate admissions processes.
+
+            </p>
+            </div>
+            <div className="col-lg-4 col-sm-12 d-flex align-items-center">
+              <h1 className="mb-4">Who We Are</h1>
+            </div>
+          </div>
+          <div className="row mb-4">
+            <div className="col-lg-4 col-sm-12 d-flex align-items-center">
+              <h1 className="mb-4">Our Mission</h1>
+            </div>
+            <div className="col-lg-8 col-sm-12">
+              <p>We strive to bridge the educational gap in today’s society; to help better the future of bright individuals through improved resources, tutoring, education, and mentorship. Smart Reach Tutoring is working to create prosperity and empower communities that do not have access to the same opportunities that other communities may have. 
+
+            </p>
+            </div>
+          </div>
+          <div className="row mb-4">
+           
+            <div className="col-lg-8 col-sm-12">
+              <p>As a result of the pandemic we recognize students are experiencing a decrease in accessible resources and academic support. With many schools transitioning to online learning, we are aware of what differentiates an in-person classroom setting from a virtual at-home setting. We understand the difficulties that accompany virtual learning, which is why we aim to ease some of the strain caused by this change in learning modes. We will be conducting all tutoring appointments through virtual meeting tools, however once restrictions have been lifted you may have the option of meeting tutors and students in person to create an even more effective educational experience.
+
+            </p>
+            </div>
+            <div className="col-lg-4 col-sm-12 d-flex align-items-center">
+              <h1 className="mb-4">COVID-19 Impact</h1>
+            </div>
+          </div> 
+          <div className="row mb-4">
+            <div className="col-lg-4 col-sm-12 d-flex align-items-center">
+              <h1 className="mb-4">Ways to Get Involved</h1>
+            </div>
+            <div className="col-lg-8 col-sm-12">
+              <p>Contact us at <strong>info@thesmartreach.org</strong> and ask about open volunteer positions! Whether you are trying to fulfill volunteer hour requirements or lend a hand by using your unique skill set to make a direct impact in a local community, we can use your help!
+            </p>
+            </div>
+          </div> 
+        </div>
+      </div>
     </div>
+    
   )
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -58,25 +58,28 @@ const SignUp = () => {
   })
   
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input onChange={handleChange} name="email" type="text" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>
-          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input onChange={handleChange} name="password" type="password" className="form-control" id="password" placeholder="Enter password"></input>
-          <small id="emailHelp" className="form-text text-muted">Your password must contain: 1) Minimum 8 characters, 2) One Uppercase Letter, 3) One Number</small>
-        </div>
-        <div className="mb-3">
-          {roleOptions}
-        </div>
-        
-        <button className="btn btn-primary">Sign Up</button>
-      </form>
+    <div className="form-page">
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email Address</label>
+              <input onChange={handleChange} name="email" type="text" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input onChange={handleChange} name="password" type="password" className="form-control" id="password" placeholder="Enter password"></input>
+              <small id="emailHelp" className="form-text text-muted">Your password must contain: 1) Minimum 8 characters, 2) One Uppercase Letter, 3) One Number</small>
+            </div>
+            <div className="mb-3">
+              {roleOptions}
+            </div>
+            
+            <button className="btn btn-primary">Sign Up</button>
+          </form>
+      </div>
     </div>
+    
   )
 }
 
