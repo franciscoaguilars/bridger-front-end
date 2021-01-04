55aecccf3147078c570152f99758943893597fe2
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-01-03 16:39:38 -0800

    Footer is now absolute positioned 230 units below bottom of viewport height to always sit at bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,39 @@
 import react from 'react';
 
 const Footer = () => {
+  const divStyle = {
+    position: 'absolute',
+    bottom: -230,
+    width: '100vw'
+  };
+
+
   return(
-    <div className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-7 col-sm-12">
-            <div className="footer-contents">
-              Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike.
+
+    <div style={divStyle}>
+      <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7 col-sm-12">
+              <div className="footer-contents">
+                Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike.
+              </div>
+            </div>
+            <div className="col-lg-5 col-sm-12">
+              <div className="footer-contact">
+                <p><strong>info@thesmartreach.org</strong></p>
+              </div>
             </div>
           </div>
-          <div className="col-lg-5 col-sm-12">
-            <div className="footer-contact">
-              <p><strong>info@thesmartreach.org</strong></p>
-            </div>
+          <div className="d-flex justify-content-center mt-5">
+              <small>© Jamieson Reinhard, Francisco Aguilar, Louis Garavaglia</small>
           </div>
+          
+          
         </div>
-        <div className="d-flex justify-content-center mt-5">
-            <small>© Jamieson Reinhard, Francisco Aguilar, Louis Garavaglia</small>
-        </div>
-        
-        
       </div>
     </div>
+    
   )
 }
 
