af5c6ee57e43f6f57e50705c6703743c775a9333
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-01-27 12:00:22 -0800

    updated footer to use JSX and custom useViewport hook for media queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,105 @@
 import react from 'react';
+import useViewport from "../hooks/useViewport";
 
 const Footer = () => {
+  const {viewportWidth} = useViewport();
   const divStyle = {
     position: 'absolute',
     bottom: -230,
     width: '100vw'
   };
+  let footer;
 
-
-  return(
-
-    <div style={divStyle}>
-      <div className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-7 col-sm-12">
-              <div className="footer-contents pr-5">
-                <img src="/images/sr_logo.png" alt="logo" width="100px" height="100px" class="mr-3" />
-                Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike.
+  if (viewportWidth > 1200) {
+    footer = (
+      <div style={divStyle}>
+        <div className="footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7 col-sm-12">
+                <div className="footer-contents pr-5">
+                  <img src="/images/sr_logo.png" alt="logo" width="100px" height="100px" class="mr-3" />
+                  Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike.
+                </div>
+              </div>
+              <div className="col-lg-5 col-sm-12">
+                <div className="footer-contact d-flex align-items-center">
+                  <p><strong>info@thesmartreach.org</strong></p>
+                </div>
               </div>
             </div>
-            <div className="col-lg-5 col-sm-12">
-              <div className="footer-contact d-flex align-items-center">
-                <p><strong>info@thesmartreach.org</strong></p>
-              </div>
+            <div className="d-flex justify-content-center mt-5">
+                <small>© The Smart Reach, 2021</small>
             </div>
           </div>
-          <div className="d-flex justify-content-center mt-5">
-              <small>© The Smart Reach, 2021</small>
-          </div>
-          
-          
         </div>
       </div>
-    </div>
-    
-  )
-}
+    );
+  } else if (viewportWidth <= 1200 && viewportWidth > 770) {
+    footer = (
+      <div style={divStyle}>
+        <div className="footer">
+          <div className="container">
+            <div className="row">
+              <div>
+                <div className="footer-contents-no-border pr-5">
+                  <img src="/images/sr_logo.png" alt="logo" width="100px" height="100px" class="mr-3" />
+                  Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike. | info@thesmartreach.org
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center mt-5">
+                <small>© The Smart Reach, 2021</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (viewportWidth <= 770 && viewportWidth > 400){
+    footer = (
+      <div style={divStyle}>
+        <div className="footer">
+          <div className="container">
+            <div className="row">
+              <div>
+                <div className="footer-contents-no-border">
+                  Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike. | info@thesmartreach.org
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center mt-5">
+                <small>© The Smart Reach, 2021</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (viewportWidth <= 400) {
+    footer = (
+      <div style={divStyle}>
+        <div className="footer">
+          <div className="container">
+            <div className="row">
+              <div>
+                <div className="footer-contents-extra-small">
+                  Smart Reach is a not-for-profit tutoring service founded on the idea that by providing educational support to economically disadvantaged students, we can bridge societal, medical, educational, and economical gaps between people, communities, and cities alike. | info@thesmartreach.org
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center mt-5">
+                <small>© The Smart Reach, 2021</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return(
+    <>
+    {footer}
+    </>
+  );
+};
 
 export default Footer;
