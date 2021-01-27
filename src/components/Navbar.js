9c1ab94dda18dd7fa84d2ac703eccd608ff97740
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-01-27 14:03:10 -0700

    added useState hook to set active nav-link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 // import getCurrentUser from '../services/services';
 import { useSelector, useDispatch } from 'react-redux';
@@ -10,6 +10,15 @@ const Navbar = () => {
   const currentUser = useSelector(store => store.user);
   const localUrl = "http://localhost:3000"
   const herokuUrl = "https://fierce-chamber-92750.herokuapp.com"
+  const navLinks = ["About", "Team", "Volunteers"]
+  const [ active, setActive  ] = useState(null);
+  const linkList = navLinks.map((link) => {
+    return(
+      <li className="nav-item" onClick={() => setActive(link)} key={link} active={active === link}>
+        <Link className={active === link ? "nav-link active" : "nav-link"} to={link.toLowerCase()}>{link}</Link>
+      </li>
+    )
+  })
 
   const logOut = () => {
     localStorage.removeItem("user");
@@ -19,70 +28,34 @@ const Navbar = () => {
     console.log(currentUser);
   };
 
-  const PageLinks = () => {
+  const Links = () => {
     if(currentUser.role) {
       return(
         <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/me">Dashboard</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tutors">Tutors</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/volunteers">Volunteers</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/team">Our Team</Link>
-            </li>
-          </ul>
-      )
-    } else {
-      return (
-        <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/team">Our Team</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/volunteers">Volunteers</Link>
-            </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="/partners">Partners</Link>
-            </li> */}
-          </ul>
-      )
-    }
-  }
-
-  const SessionLinks = () => {
-    if(currentUser.role) {
-      return(
-        <ul className="navbar-nav ml-auto">
-            <li className="dropdown">
+            {linkList}
+            {/* <li className="dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <img className="nav-avatar" alt="logout" src={`${herokuUrl}/${currentUser.avatar}`}/>
               </a>
               <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <Link className="dropdown-item" to="/signin" onClick={logOut}> Logout</Link>
               </div>
-            </li>
-        </ul>
+            </li> */}
+          </ul>
       )
     } else {
-      return(
+      return (
         <ul className="navbar-nav ml-auto">
+            {linkList}
+            {/* <li className="nav-item">
+              <Link onClick={setActive()} className="nav-link" to="/partners">Partners</Link>
+            </li>
             <li className="nav-item">
              <Link className="nav-link" to="/signin">Log In</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/signup">Sign Up</Link>
-            </li>
+            </li> */}
           </ul>
       )
     }
@@ -91,13 +64,15 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light">
-      <Link className="navbar-brand d-flex align-items-center" to="/" ><img src="images/sr_logo.png" width="50px" height="50px" alt="logo" class="mr-1" />Smart Reach</Link>
+      <Link className="navbar-brand d-flex align-items-center" to="/" onClick={() => setActive(null)}>
+        <img src="images/sr_logo.png" width="50px" height="50px" alt="logo" class="mr-1" />
+        Smart Reach
+      </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <PageLinks />
-          {/* <SessionLinks /> */}
+          <Links />
         </div>
       </nav>
     </div>
